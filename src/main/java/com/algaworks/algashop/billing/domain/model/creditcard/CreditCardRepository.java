2a2cc8322bd8b6531e6b546ba6faa2e44f38992b
anailson <arsantos315@gmail.com>
2026-06-28 17:17:04 -0300

    Application Service com CQRS para geração de Invoice

--- src/main/java/com/algaworks/algashop/billing/domain/model/creditcard/CreditCardRepository.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/creditcard/CreditCardRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CreditCarRepository extends JpaRepository<CreditCard, UUID> {
+public interface CreditCardRepository extends JpaRepository<CreditCard, UUID> {
 }
